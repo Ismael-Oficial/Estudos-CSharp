7a811cb34043bdf0b6bb7080256eca210789263a
Ismael Chaves Mizael <125465461+Ismael-Oficial@users.noreply.github.com>
2023-07-25 17:41:17 -0300

    Primeiro Programa em C#

---
+++ PrimeiroPrograma/PrimeiroPrograma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrimeiroCodigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Qual é a sua idade? ");
+            String idadeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Olá, Seja Bem-Vindo " + nomeUsuario + " Sua Idade é: " + idadeUsuario);
+
+            Console.ReadLine();
+        }
+    }
+}
