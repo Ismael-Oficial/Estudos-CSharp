0a517cb01c53374dfa729cd6ee5dfff9ba01a335
Ismael Chaves Mizael <125465461+Ismael-Oficial@users.noreply.github.com>
2023-07-25 17:25:48 -0300

    Primeiro Código em C#

---
+++ PrimeiroCodigo/PrimeiroCodigo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroCodigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Esse é meu Primeiro Código em C#");
+            Console.ReadLine();
+        }
+    }
+}
