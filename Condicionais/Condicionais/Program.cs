59a3c98c2ae24e893fe702dde78541574aad1d9b
Ismael Chaves Mizael <125465461+Ismael-Oficial@users.noreply.github.com>
2023-07-31 14:47:41 -0300

    Add files via upload

---
+++ Condicionais/Condicionais/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade < 18)
+            {
+                Console.WriteLine("Usuário Menor de Idade");
+            }
+            else
+            {
+                Console.WriteLine("Usuário Maior de Idade");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
